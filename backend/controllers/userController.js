ebf0465a1293d5bc00bd57d2358db7afbd7bdd8c
deep56-dot <72162650+deep56-dot@users.noreply.github.com>
2024-11-20 00:25:05 +0530

    minor bug fixes for deployment

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -101,6 +101,8 @@ export const register = AsyncHandler(async (req, res, next) => {
     .cookie("token", "", {
       expires: new Date(Date.now()),
       httpOnly: true,
+      secure:true,
+      sameSite: "None"
     })
     .json({
       success: true,
