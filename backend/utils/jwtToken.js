cf072fd03c61e1ad04544d9f2689044bfdc14f69
deep56-dot <72162650+deep56-dot@users.noreply.github.com>
2024-11-20 10:56:33 +0530

    bug fixes for ui

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,12 +1,14 @@
 export const sendToken = (user, statusCode, res, message) => {
     const token = user.getJWTToken();
+    const domain = '.jobnode.netlify.app';
     const options = {
       expires: new Date(
         Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
       ),
       httpOnly: true,
       secure:true,
-      sameSite: "None"
+      sameSite: "None",
+      domain: domain
     };
   
     res.status(statusCode).cookie("token", token, options).json({
